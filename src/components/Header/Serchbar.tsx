5bf4ff36aa593a8eeba3c0b8ba92198c730be8fc
Mahdi <mahdibagherichanel@gmail.com>
2025-05-13 20:48:14 +0330

    add nprogress

--- src/components/Header/Serchbar.tsx
+++ src/components/Header/Serchbar.tsx
@@ -1,7 +1,10 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 
 function Searchbar() {
+  const nav = useRouter();
   return (
     <div className="w-full h-[44px] sm:w-[439px] bg-gray-200 rounded-xl flex items-center px-4 dir-rtl mr-2">
       <button className="mr-2 text-gray-600">
@@ -9,6 +12,7 @@ function Searchbar() {
       </button>
       <input
         type="text"
+        onClick={() => nav.push("/search")}
         placeholder="جستجو کسب‌ و کار"
         className="w-full h-full bg-transparent border-none outline-none text-right text-gray-700 placeholder-gray-500"
       />
