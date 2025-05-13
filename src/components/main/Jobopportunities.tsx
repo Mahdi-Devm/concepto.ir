94ed07318a23f48c5f8db25913b72c6761aac5a0
Mahdi <mahdibagherichanel@gmail.com>
2025-05-13 20:24:20 +0330

    add page for search bar

--- src/components/main/Jobopportunities.tsx
+++ src/components/main/Jobopportunities.tsx
@@ -9,18 +9,20 @@ import { jobs } from "@/context/jobs";
 
 export default function Jobopportunities() {
   return (
-    <section aria-label="فرصت‌های شغلی" className="py-10">
+    <section aria-label="فرصت‌های شغلی" className="py-10 w-100 md:w-full">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-center gap-4 mb-6">
           <hr className="border border-stone-300 w-32" />
-          <h2 className="text-xl font-semibold text-gray-800">فرصت‌های شغلی</h2>
+          <h2 className="text-2xl font-semibold text-gray-800">
+            فرصت‌های شغلی
+          </h2>
           <hr className="border border-stone-300 w-32" />
         </div>
 
         <Swiper
           modules={[Autoplay]}
+          slidesPerView={2}
           spaceBetween={20}
-          slidesPerView={1}
           loop={true}
           autoplay={{
             delay: 2500,
@@ -28,32 +30,33 @@ export default function Jobopportunities() {
           }}
           breakpoints={{
             640: {
-              slidesPerView: 2,
+              slidesPerView: 1, // نمایش 1 اسلاید در موبایل
+              spaceBetween: 10, // فاصله کمتر بین اسلایدها
             },
             768: {
-              slidesPerView: 3,
+              slidesPerView: 2, // نمایش 2 اسلاید در تبلت
             },
             1024: {
-              slidesPerView: 4,
+              slidesPerView: 3, // نمایش 3 اسلاید در دسکتاپ
             },
             1280: {
-              slidesPerView: 5,
+              slidesPerView: 5, // نمایش 4 اسلاید در صفحات بزرگتر
             },
           }}
         >
           {jobs.map((job) => (
             <SwiperSlide key={job.id}>
               <article
-                className="rounded-xl bg-[#F6F6F6] shadow-md overflow-hidden p-4 flex flex-col h-70"
+                className="rounded-xl h-80 md:h-80 bg-[#F6F6F6] shadow-md overflow-hidden p-4 flex flex-col max-w-sm mx-auto"
                 itemScope
                 itemType="https://schema.org/JobPosting"
               >
                 <Image
                   width={300}
-                  height={40}
+                  height={200}
                   src={job.image}
                   alt={`تصویر موقعیت شغلی ${job.title} در ${job.company}`}
-                  className="w-60 h-30 object-cover rounded-lg mb-3"
+                  className="w-full mt-5 h-20 md:h-25 object-cover rounded-lg mb-3"
                   itemProp="image"
                 />
                 <h3
