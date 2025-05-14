b626376b0935ae3c6cdfd84a31e0aad9d4d9e368
Mahdi <mahdibagherichanel@gmail.com>
2025-05-14 15:49:50 +0330

    login page

--- src/components/Login-Btn/Login.tsx
+++ src/components/Login-Btn/Login.tsx
@@ -1,13 +1,16 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { CiLogin } from "react-icons/ci";
 
 function LoginBtn({ mobileView = false }) {
+  const nav = useRouter();
   return (
     <button
       className={`btnnotblue border-1 flex justify-center items-center rounded-md ${
         mobileView ? "w-[40px] h-[40px]" : "w-[184px] h-[48px]"
       }`}
+      onClick={() => nav.push("/signup")}
     >
       {!mobileView && <span className="ml-2">ورود/ثبت نام</span>}
       <CiLogin className="text-2xl" />
