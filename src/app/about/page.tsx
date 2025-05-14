e078cab1dd677d2d58e69f374f596eff8decebf9
Mahdi <mahdibagherichanel@gmail.com>
2025-05-14 15:50:50 +0330

    Contact page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import Head from "next/head";
+import StorySection from "@/components/about/StorySection";
+import Headerabotpagetext from "@/components/about/Headerabotpagetext";
+import Memberaboutpage from "@/components/about/Memberaboutpage";
+import BoxDetail from "@/components/about/BoxDetail";
 
 function About() {
   return (
@@ -9,7 +13,12 @@ function About() {
         <meta name="description" content="درباره تیم ما بیشتر بدانید." />
         <meta name="keywords" content="درباره ما, اطلاعات تیم, ما کی هستیم" />
       </Head>
-      <div>About</div>
+      <section>
+        <Headerabotpagetext />
+        <BoxDetail />
+        <StorySection />
+        <Memberaboutpage />
+      </section>
     </>
   );
 }
